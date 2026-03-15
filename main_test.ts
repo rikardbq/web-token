8017ac898b3fc01e1c89735275ae792b77bba707
rikardbq <j10rikardb@gmail.com>
2026-03-15 22:11:58 +0100

    start to add some form of tests

--- main_test.ts
+++ main_test.ts
@@ -1,34 +1,26 @@
 // deno-lint-ignore-file no-explicit-any
-import { assertEquals } from "@std/assert";
+import { assertEquals, assert } from "@std/assert";
 import { AuthToken, exportKeyPair, generateKeyPair } from "./main.ts";
-// import { add } from "./main.ts";
 
-// Deno.test(function addTest() {
-//   assertEquals(add(2, 3), 5);
-// });
+const { publicKeyJwk, privateKeyPkcs8 } = await exportKeyPair(
+    await generateKeyPair(),
+);
+const iat = 1773607866860;
+const exp = iat + 30000;
 
-const keyPair = await generateKeyPair();
-const { publicKeyJwk, privateKeyPkcs8 } = await exportKeyPair(keyPair);
-export const encodeToken = async (
+const encodeToken = async (
     type: "id" | "at" | "rt",
     aid: string,
     sub: string,
     extra: any = {},
 ) => {
-    const exp = () => {
-        const now = Date.now();
-        if (type === "at") return now + 1800 * 1000;
-        if (type === "rt") return now + 3600 * 1000 * 24 * 30;
-
-        return now + 30 * 1000;
-    };
-
     const token = AuthToken.AuthTokenBuilder()
         .withType(type)
         .withAid(aid)
-        .withIss("issuer")
+        .withIss("test_issuer")
         .withSub(sub)
-        .withExp(exp())
+        .withIat(iat)
+        .withExp(exp)
         .withExtra(extra);
 
     return await token.sign(
@@ -39,7 +31,7 @@ export const encodeToken = async (
     );
 };
 
-export const decodeToken = async (
+const decodeToken = async (
     b64Token: string,
     type: "id" | "at" | "rt",
     sub: string,
@@ -48,7 +40,9 @@ export const decodeToken = async (
     const token = AuthToken.AuthTokenVerifier(b64Token)
         .withType(type)
         .withAid("test")
-        .withIss("issuer")
+        .withIss("test_issuer")
+        .withIat(iat)
+        .withExp(exp)
         .withSub(sub);
 
     claims.forEach(([k, v]) => {
@@ -63,12 +57,39 @@ export const decodeToken = async (
     );
 };
 
-const token = await encodeToken("id", "test", "ident", {
-    something: "some",
-    something2: "some2",
-    tee: "tester",
-});
+Deno.test(
+    "should encode token - produce valid base64 body and footer",
+    async () => {
+        const expectedTokenJson = JSON.stringify({
+            tt: "id",
+            aid: "test",
+            iss: "test_issuer",
+            sub: "ident",
+            iat: iat,
+            exp: exp,
+            test_claim: "tester",
+        });
+        const token = await encodeToken("id", "test", "ident", {
+            test_claim: "tester",
+        });
+        const splitToken = token.split(".");
+        const decoder = new TextDecoder();
 
-const verify = await decodeToken(token, "id", "ident", ["something"], ["something2", "some2"], ["tee", "tester"]);
-
-console.log(verify);
+        assert(splitToken.length === 2);
+        assertEquals(
+            decoder.decode(Uint8Array.fromBase64(splitToken[0])),
+            expectedTokenJson,
+        );
+        assert(
+            await crypto.subtle.verify(
+                "Ed25519",
+                (await AuthToken.importKey({
+                    key: publicKeyJwk,
+                    type: "public",
+                }))!,
+                Uint8Array.fromBase64(splitToken[1]),
+                Uint8Array.fromBase64(splitToken[0]),
+            ),
+        );
+    },
+);
